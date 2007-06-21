4aa891dc971e24f3ea52a4cc3e8c9376ee35bfe5
Carlos Eduardo Lara Augusto <augusto@tecgraf.puc-rio.br>
2007-06-21 02:49:52 +0000

    Some minor bug corrections. Better code legibility.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/scs/trunk/src/lua/scs/core@64686 ae0415b3-e90b-0410-900d-d0be9363c56b

--- base.lua
+++ base.lua
@@ -1,4 +1,9 @@
-local verbose = require "oil.verbose"
+--
+-- SCS
+-- base.lua
+-- Description: Basic SCS classes and API
+-- Version: 1.0
+--
 
 local oo        = require "loop.base"
 local component = require "loop.component.base"
@@ -26,31 +31,44 @@ module "scs.core.base"
 
 --------------------------------------------------------------------------------
 
-IsMultipleReceptacle = {
+-- This structure is used to check the type of the receptacle
+local IsMultipleReceptacle = {
 	[ports.HashReceptacle] = true,
 	[ports.ListReceptacle] = true,
 	[ports.SetReceptacle] = true,
 }
 
+--
+-- Description: Creates a new component instance and prepares it to be used in the system.
+-- Parameter factory: Factory object that will be used to create the instance.
+-- Parameter descriptions: Table with the facet and receptacle descriptions for the component. 
+-- 						   Indexed by the name of the port.
+-- Return Value: New LOOP component as specified by the factory's template. Nil if something 
+--				 goes wrong.
+--
 function newComponent(factory, descriptions)
 	local instance = factory()
-	instance.facetDescs = {}
-	instance.receptacleDescs = {}
-	instance.receptsByConId = {}
-	instance.numConnections = 0
-	instance.nextConnId = 0
-	instance.maxConnections = 100
+	if not instance then
+		return nil
+	end
+	instance._facetDescs = {}
+	instance._receptacleDescs = {}
+	instance._receptsByConId = {}
+	instance._numConnections = 0
+	instance._nextConnId = 0
+	instance._maxConnections = 100
 	for name, kind in component.ports(instance) do
 		if kind == ports.Facet then
-			instance.facetDescs[name] = descriptions[name]
-			instance.facetDescs[name].facet_ref = oil.newobject(instance[name], descriptions[name].interface_name)
-			instance[name] = instance.facetDescs[name].facet_ref
+			instance._facetDescs[name] = descriptions[name]
+			instance._facetDescs[name].facet_ref = oil.newobject(instance[name], 
+												   descriptions[name].interface_name)
+			instance[name] = instance._facetDescs[name].facet_ref
 		elseif kind == ports.Receptacle or IsMultipleReceptacle[kind] then
-			if not descriptions[name].connections then
-				descriptions[name].connections = {}
+			if not descriptions[name]._connections then
+				descriptions[name]._connections = {}
 			end
-			descriptions[name].keys = {}
-			instance.receptacleDescs[name] = descriptions[name]
+			descriptions[name]._keys = {}
+			instance._receptacleDescs[name] = descriptions[name]
 		end
 	end
 	return instance
@@ -58,23 +76,45 @@ end
 
 --------------------------------------------------------------------------------
 
+--
+-- Component Class
+-- Implementation of the IComponent Interface from scs.idl
+--
 Component = oo.class{ context = false }
 
+--
+-- Description: Does nothing initially. Will probably receive another implementation by the
+-- 				application component's developer.
+--
 function Component:startup()
 end
 
+--
+-- Description: Does nothing initially. Will probably receive another implementation by the
+-- 				application component's developer.
+--
 function Component:shutdown()
 end
 
+--
+-- Description: Provides a specific interface's object.
+-- Parameter interface: The desired interface.
+-- Return Value: The CORBA object that implements the interface. 
+--
 function Component:getFacet(interface)
 	self = self.context
 	for name, kind in component.ports(self) do
-		if kind == ports.Facet and self.facetDescs[name].interface_name == interface then
+		if kind == ports.Facet and self._facetDescs[name].interface_name == interface then
 			return self[name]
 		end
 	end
 end
 
+--
+-- Description: Provides a specific interface's object.
+-- Parameter interface: The desired interface's name.
+-- Return Value: The CORBA object that implements the interface. 
+--
 function Component:getFacetByName(name)
 	self = self.context
 	if component.templateof(self)[name] == ports.Facet then
@@ -84,8 +124,19 @@ end
 
 --------------------------------------------------------------------------------
 
+--
+-- Receptacles Class
+-- Implementation of the IReceptacles Interface from scs.idl
+--
 Receptacles = oo.class{ context = false }
 
+--
+-- Description: Connects an object to the specified receptacle.
+-- Parameter receptacle: The receptacle's name that corresponds to the interface implemented by the
+-- 						 provided object.
+-- Parameter object: The CORBA object that implements the expected interface.
+-- Return Value: The connection's identifier.
+--
 function Receptacles:connect(receptacle, object)
 	if not object then error{ "IDL:scs/core/InvalidConnection:1.0" } end
 	self = self.context
@@ -101,29 +152,34 @@ function Receptacles:connect(receptacle, object)
 		end
 	elseif IsMultipleReceptacle[port] then
 		-- this receptacle accepts multiple connections
-		-- in the case of a HashReceptacle, we must provide an identifier, which will be the connection's id.
+		-- in the case of a HashReceptacle, we must provide an identifier, which will be the 
+		-- connection's id.
 		-- if it's not a HashReceptacle, it'll ignore the provided identifier
-		bindKey = self[receptacle]:__bind(object, (self.nextConnId + 1))
+		bindKey = self[receptacle]:__bind(object, (self._nextConnId + 1))
 	else
 		error{ "IDL:scs/core/InvalidName:1.0" }
 	end
-	if (self.numConnections <= self.maxConnections) then
-		self.numConnections = self.numConnections + 1
-		self.nextConnId = self.nextConnId + 1
-		self.receptacleDescs[receptacle].connections[self.nextConnId] = {	id = self.nextConnId, 
-																			objref = object }
-		self.receptsByConId[self.nextConnId] = self.receptacleDescs[receptacle]
+	if (self._numConnections <= self._maxConnections) then
+		self._numConnections = self._numConnections + 1
+		self._nextConnId = self._nextConnId + 1
+		self._receptacleDescs[receptacle]._connections[self._nextConnId] = { id = self._nextConnId, 
+																			 objref = object }
+		self._receptsByConId[self._nextConnId] = self._receptacleDescs[receptacle]
 		if bindKey > 0 then
-			self.receptacleDescs[receptacle].keys[self.nextConnId] = bindKey
+			self._receptacleDescs[receptacle]._keys[self._nextConnId] = bindKey
 		end
-		return self.nextConnId
+		return self._nextConnId
 	end
 	error{ "IDL:scs/core/ExceededConnectionLimit:1.0" }
 end
 
+--
+-- Description: Disconnects an object from a receptacle.
+-- Parameter connId: The connection's identifier.
+--
 function Receptacles:disconnect(connId)
 	self = self.context
-	receptacle = self.receptsByConId[connId].name
+	receptacle = self._receptsByConId[connId].name
 	local port = component.templateof(self)[receptacle]
 	if port == ports.Receptacle then
 		if self[receptacle] then
@@ -132,11 +188,11 @@ function Receptacles:disconnect(connId)
 			error{ "IDL:scs/core/InvalidConnection:1.0" }
 		end
 	elseif IsMultipleReceptacle[port] then
-		if self[receptacle]:__unbind(self.receptacleDescs[receptacle].keys[connId]) then
-			self.numConnections = self.numConnections - 1
-			self.receptacleDescs[receptacle].connections[connId] = nil
-			self.receptsByConId[connId].connections[connId] = nil
-			self.receptacleDescs[receptacle].keys[connId] = nil
+		if self[receptacle]:__unbind(self._receptacleDescs[receptacle]._keys[connId]) then
+			self._numConnections = self._numConnections - 1
+			self._receptacleDescs[receptacle]._connections[connId] = nil
+			self._receptsByConId[connId]._connections[connId] = nil
+			self._receptacleDescs[receptacle]._keys[connId] = nil
 		else
 			error{ "IDL:scs/core/InvalidConnection:1.0" }
 		end
@@ -145,38 +201,54 @@ function Receptacles:disconnect(connId)
 	end
 end
 
+--
+-- Description: Provides information about all the current connections of a receptacle.
+-- Parameter receptacle: The receptacle's name.
+-- Return Value: All current connections of the specified receptacle.
+--
 function Receptacles:getConnections(receptacle)
 	self = self.context
-	if self.context.receptacleDescs[receptacle] then
-		return self.receptacleDescs[receptacle].connections
+	if self.context._receptacleDescs[receptacle] then
+		return self._receptacleDescs[receptacle]._connections
 	end
 	error{ "IDL:scs/core/InvalidName:1.0", name = receptacle }
 end
 
 --------------------------------------------------------------------------------
 
+--
+-- MetaInterface Class
+-- Implementation of the IMetaInterface Interface from scs.idl
+--
 MetaInterface = oo.class{ context = false }
 
-function MetaInterface:getDescriptions(portType, selected)
+--
+-- Description: Provides descriptions for one or more ports.
+-- Parameter portType: Type of the port. May be facet or receptacle.
+-- Parameter selected: Names of the ports. If nil, descriptions for all ports of the type will be
+--					   returned.
+-- Return Value: The descriptions that apply.
+--
+local function MetaInterface:getDescriptions(portType, selected)
 	self = self.context
 	if not selected then
 		if portType == "receptacle" then
-			return self.receptacleDescs
+			return self._receptacleDescs
 		elseif portType == "facet" then
-			return self.facetDescs
+			return self._facetDescs
 		end
 	end
 	local descs = {}
 	for _, name in ipairs(selected) do
 		if portType == "receptacle" then
-			if self.receptacleDescs[name] then
-				descs[name] = self.receptacleDescs[name]
+			if self._receptacleDescs[name] then
+				descs[name] = self._receptacleDescs[name]
 			else
 				error{ "IDL:scs/core/InvalidName:1.0", name = name }
 			end
 		elseif portType == "facet" then
-			if self.facetDescs[name] then
-				descs[name] = self.facetDescs[name]
+			if self._facetDescs[name] then
+				descs[name] = self._facetDescs[name]
 			else
 				error{ "IDL:scs/core/InvalidName:1.0", name = name }
 			end
@@ -186,26 +258,47 @@ function MetaInterface:getDescriptions(portType, selected)
 end
 
 
+--
+-- Description: Provides descriptions for all the facets.
+-- Return Value: The descriptions.
+--
 function MetaInterface:getFacets()
 	return self:getDescriptions("facet")
 end
 
+--
+-- Description: Provides descriptions for one or more facets.
+-- Parameter names: Names of the facets.
+-- Return Value: The descriptions that apply.
+--
 function MetaInterface:getFacetsByName(names)
 	return self:getDescriptions("facet", names)
 end
 
+--
+-- Description: Provides descriptions for all the receptacles.
+-- Return Value: The descriptions.
+--
 function MetaInterface:getReceptacles()
 	return self:getDescriptions("receptacle")
 end
 
+--
+-- Description: Provides descriptions for one or more receptacles.
+-- Parameter names: Names of the receptacles.
+-- Return Value: The descriptions that apply.
+--
 function MetaInterface:getReceptaclesByName(names)
 	return self:getDescriptions("receptacle", names)
 end
 
 --------------------------------------------------------------------------------
 
-Utils = oo.class{ 	context 	= false,
-					verbose 	= false,
+--
+-- Util Class
+-- Implementation of the utilitary class. It's use is not mandatory.
+--
+Utils = oo.class{ 	verbose 	= false,
 					fileVerbose = false,
 					newLog		= true,
 					fileName 	= "",
@@ -220,7 +313,8 @@ function Utils:verbosePrint(message)
 		print(message)
 	end
 	if self.fileVerbose then
-		local f = assert(io.open("../../../../logs/lua/"..self.fileName.."/"..self.fileName..".log", "a"))
+		local f = assert(io.open("../../../../logs/lua/"..self.fileName.."/"..self.fileName..".log",
+								 "a"))
 		if self.newLog then
 			f:write("\n-----------------------------------------------------\n")
 			f:write(os.date().." "..os.time().."\n")

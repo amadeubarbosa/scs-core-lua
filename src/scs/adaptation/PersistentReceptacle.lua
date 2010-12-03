5b4e61c04763b8c23a6345995a9efb2a976fff32
Maíra Gatti <mgatti@tecgraf.puc-rio.br>
2010-12-03 19:10:16 +0000

    [SCS-219]  Criação do Receptaculo Persistente no SCS Demanda criada a partir da issue [OPENBUS-1098].
    --versão correta
    
    
    
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/scs/core/lua/branches/SCS_CORE_LUA_v1_02_2010_09_21@113339 ae0415b3-e90b-0410-900d-d0be9363c56b

--- src/scs/adaptation/PersistentReceptacle.lua
+++ src/scs/adaptation/PersistentReceptacle.lua
@@ -1,16 +1,20 @@
-
+local ipairs = ipairs
+local assert = assert
 local scs = require "scs.core.base"
 local oop = require "loop.simple"
 local print = print
 local pairs = pairs
 local tostring = tostring
+local type = type
+local tonumber = tonumber
+local error = error
 
 local oil = require "oil"
 local orb = oil.orb
 
 local utils     = require "scs.core.utils"
-local TableDB       = require "scs.util.TableDB"
 local OilUtilities = require "scs.adaptation.OilUtilities"
+local AdaptiveReceptacle = require "scs.adaptation.AdaptiveReceptacle"
 
 module("scs.adaptation.PersistentReceptacle")
 
@@ -19,11 +23,28 @@ module("scs.adaptation.PersistentReceptacle")
 -- Implementation of the IReceptacles Interface from scs.idl
 --
 
-PersistentReceptacleFacet = oop.class({}, scs.adaptation.AdaptiveReceptacle)
+PersistentReceptacleFacet = oop.class({}, AdaptiveReceptacle.AdaptiveReceptacleFacet)
 
-function PersistentReceptacleFacet:__init(dbfile)
-  self = scs.adaptation.AdaptiveReceptacle.__init(self)
-  self.connectionsDB = TableDB(dbfile)
+
+-- Description: Creates an instance of the receptacle.
+-- Parameter dbmanager: represents the persistent db manager.
+--           This manager must implement 'save', 'remove', and 'get'
+--           SCS provides an implementation for this manager.
+--           Check the file /util/TableDB.lua
+-- Example of instantiation and usage during Component configuration:
+--   local receptFacetRef = orb:newservant(MyService.ReceptacleFacet(TableDB(dbfile)),
+--                                        "","IDL:scs/core/IReceptacles:1.0")
+--   facetDescriptions.IReceptacles.facet_ref      = receptFacetRef
+
+function PersistentReceptacleFacet:__init(dbmanager)
+  --Checks if dbmanager implements the required operations
+  if type(dbmanager.save) ~= "function"   or
+     type(dbmanager.remove) ~= "function" or
+     type(dbmanager.get) ~= "function"    then
+     error ( orb:newexcept{"CORBA::PERSIST_STORE"}[1] )
+  end
+  self = AdaptiveReceptacle.AdaptiveReceptacleFacet.__init(self)
+  self.connectionsDB = dbmanager
   --used to load data during getConnections at the first time
   self.firstRequired = true
   return self
@@ -32,11 +53,11 @@ end
 function PersistentReceptacleFacet:connect(receptacle, object)
   self.utils:verbosePrint("[PersistentReceptacleFacet:connect]")
 
-  local connId = scs.adaptation.AdaptiveReceptacle.connect(self, receptacle, object)
+  local connId = AdaptiveReceptacle.AdaptiveReceptacleFacet.connect(self, receptacle, object)
   if type(connId) == "number" then
     if not self.connectionsDB:get(connId) then
     --saves onle if it is not already saved
-      self.connectionsDB:save(connId, orb:tostring(object))
+      self.connectionsDB:save(tonumber(connId), orb:tostring(object))
     end
   end
   return connId
@@ -54,7 +75,7 @@ function PersistentReceptacleFacet:disconnect(connId)
   --removes only if exists
     self.connectionsDB:remove(connId)
   end
-  return scs.adaptation.AdaptiveReceptacle.disconnect(self,connId) -- calling inherited method
+  return AdaptiveReceptacle.AdaptiveReceptacleFacet.disconnect(self,connId) -- calling inherited method
 end
 
 function PersistentReceptacleFacet:getConnections(receptacle)
@@ -63,15 +84,19 @@ function PersistentReceptacleFacet:getConnections(receptacle)
     -- Load the connections
     local data = assert(self.connectionsDB:getValues())
     for connId, objIOR in ipairs(data) do
-      local object = newproxy(objIOR)
-      local newConnId = scs.adaptation.AdaptiveReceptacle.connect(self, receptacle, object)
-      if newConnId ~= connId then
-        --update the connId only if the new one is different from the one saved
-        self.connectionsDB:remove(connId, object)
-        self.connectionsDB:save(newConnId, object)
+      local object = orb:newproxy(objIOR, "synchronous", oil.corba.idl.object)
+      if OilUtilities:existent(object) then
+        local newConnId = self:connect(receptacle, object)
+        if newConnId ~= connId then
+          --update the connId only if the new one is different from the one saved
+          self.connectionsDB:remove(connId)
+          self.connectionsDB:save(tonumber(newConnId), objIOR)
+        end
+      else
+         self.connectionsDB:remove(connId)
       end
     end
     self.firstRequired = false
   end
-  return scs.adaptation.AdaptiveReceptacle.getConnections(self,receptacle) -- calling inherited method
+  return AdaptiveReceptacle.AdaptiveReceptacleFacet.getConnections(self,receptacle) -- calling inherited method
 end

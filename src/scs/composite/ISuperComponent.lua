c006ab7219c17c2fc64fa2909b21cb887ff3192a
Mauricio Arieira Rosas <marosas@tecgraf.puc-rio.br>
2013-01-28 00:38:51 +0000

    formataca
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/scs/core/lua/branches/SCS_CORE_LUA_COMPOSITE_2012_10_06@137534 ae0415b3-e90b-0410-900d-d0be9363c56b

--- src/scs/composite/ISuperComponent.lua
+++ src/scs/composite/ISuperComponent.lua
@@ -1,12 +1,4 @@
 
-
----
--- SCS
--- ISuperComponent.lua
--- Description:  ... . .. . . . . .
--- Version: 1.0
----
-
 local oo = require "loop.base"
 local class = oo.class
 local pairs = pairs
@@ -19,53 +11,53 @@ utils = utils()
 local ISuperComponent = class()
 
 function ISuperComponent:__new()
- 
+
   return oo.rawnew(self, {superComponents = {}})
 end
 
 
 function ISuperComponent:addSuperComponent(iComponent)
-	local context = self.context
-	local composite = context._orb:narrow(iComponent:getFacetByName(utils.ICONTENTCONTROLLER_NAME))
+  local context = self.context
+  local composite = context._orb:narrow(iComponent:getFacetByName(utils.ICONTENTCONTROLLER_NAME))
 
   if not iComponent:_is_a(utils.ICOMPONENT_INTERFACE) then
     error { compositeIdl.throw.InvalidComponent }
   end
 
-	self.superComponents[composite:getId()] = iComponent
+  self.superComponents[composite:getId()] = iComponent
 end
 
 
 function ISuperComponent:removeSuperComponent(iComponent)
-	local context = self.context
+  local context = self.context
   local composite = context._orb:narrow(iComponent:getFacetByName(utils.ICONTENTCONTROLLER_NAME))
-  
+
   local ok, compositeId = pcall(composite.getId, composite)
   if not ok or not compositeId then
     Log:error("ID não retornado pela funcao getId().")
     return false
-  end 
-  
+  end
+
   local remComponent = self.superComponents[compositeId]
   if not remComponent then
     Log:error(string.format("O id '%s' não existe", composite:getId()))
     return false
-  end 
-  
+  end
+
   self.superComponents[composite:getId()] = nil
   return true
 end
 
 
 function ISuperComponent:getSuperComponents()
-	local context = self.context
-	local superComponents = {}
+  local context = self.context
+  local superComponents = {}
 
-	for _, icomponent in pairs(self.superComponents) do
-		table.insert(superComponents, icomponent)
-	end
+  for _, icomponent in pairs(self.superComponents) do
+    table.insert(superComponents, icomponent)
+  end
 
-	return superComponents
+  return superComponents
 end
 
 

ff9bcc38167618e0389023dfe807b013e2f514f0
Mauricio Arieira Rosas <marosas@tecgraf.puc-rio.br>
2013-02-16 17:59:19 +0000

    formatacao
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/scs/core/lua/branches/SCS_CORE_LUA_COMPOSITE_2012_10_06@137988 ae0415b3-e90b-0410-900d-d0be9363c56b

--- demos/composite/compositeClient.lua
+++ demos/composite/compositeClient.lua
@@ -13,24 +13,24 @@ local helloFacetName = "Hello"
 local helloFacetInterface = "IDL:scs/demos/helloworld/Hello:1.0"
 
 oil.main(function()
-	local idlPath = os.getenv("IDL_PATH")
+  local idlPath = os.getenv("IDL_PATH")
   orb:loadidlfile(idlPath .. "/scs.idl")
   orb:loadidlfile(idlPath .. "/composite.idl")
-  orb:loadidlfile(idlPath .. "/hello.idl")  
+  orb:loadidlfile(idlPath .. "/hello.idl")
   oil.newthread(orb.run, orb)
 
-	--Getting proxy to primitive component
-	local serverIOR = oil.readfrom("server.ior")
-	local compositeComponent = orb:newproxy(serverIOR)
-	
-	local helloFacet = compositeComponent:getFacetByName("IHelloX")
-	helloFacet = orb:narrow(helloFacet, helloFacetInterface)
-	if not helloFacet then 
-		print("[ERROR] Faceta não encontrada.")
-		os.exit(1)
-	end
-	
-	helloFacet:sayHello()
-	return
+  --Getting proxy to primitive component
+  local serverIOR = oil.readfrom("server.ior")
+  local compositeComponent = orb:newproxy(serverIOR)
+
+  local helloFacet = compositeComponent:getFacetByName("IHelloX")
+  helloFacet = orb:narrow(helloFacet, helloFacetInterface)
+  if not helloFacet then
+    print("[ERROR] Faceta não encontrada.")
+    os.exit(1)
+  end
+
+  helloFacet:sayHello()
+  return
 end)
 
--- demos/composite/compositeServer.lua
+++ demos/composite/compositeServer.lua
@@ -11,10 +11,10 @@ Log:level(5)
 --implementação da faceta IHello
 local Hello = oo.class{}
 function Hello:__new(name)
-	return oo.rawnew(self,{name = name})
+  return oo.rawnew(self,{name = name})
 end
 function Hello:sayHello()
-	print("Hello " .. self.name .. "!")
+  print("Hello " .. self.name .. "!")
 end
 
 -- OiL configuration
@@ -22,38 +22,38 @@ local orb = oil.init({localrefs = "proxy"})
 local helloFacetInterface = "IDL:scs/demos/helloworld/Hello:1.0"
 
 oil.main(function()
-	local idlPath = os.getenv("IDL_PATH")
+  local idlPath = os.getenv("IDL_PATH")
   orb:loadidlfile(idlPath .. "/scs.idl")
   orb:loadidlfile(idlPath .. "/composite.idl")
   orb:loadidlfile(idlPath .. "/hello.idl")
   oil.newthread(orb.run, orb)
 
-  -- Cria dois componentes Hello  
+  -- Cria dois componentes Hello
   local componentId = { name = "Hello1", major_version = 1, minor_version = 0, patch_version = 0, platform_spec = "" }
   local helloComponent1 = ComponentContext(orb, componentId)
-  helloComponent1:addFacet("IHello1", helloFacetInterface, Hello("SubComponent One"))	
-  
+  helloComponent1:addFacet("IHello1", helloFacetInterface, Hello("SubComponent One"))
+
   local componentId = { name = "Hello2", major_version = 1, minor_version = 0, patch_version = 0, platform_spec = "" }
   local helloComponent2 = ComponentContext(orb, componentId)
   helloComponent2:addFacet("IHello2", helloFacetInterface, Hello("SubComponent Two"))
 
-	-- Cria o componente composto
-	local componentId = { name = "ComplexHello", major_version = 1, minor_version = 0, patch_version = 0, platform_spec = "" }
-	local component = ComponentContext(orb, componentId)
-	local icontentController = component:getFacetByName(utils.ICONTENTCONTROLLER_NAME).facet_ref
-	
-	-- Adiciona os componentes Hello no Componente Composto e cria uma faceta.
-	local membershipID1 = icontentController:addSubComponent(helloComponent1:getIComponent())
-	local membershipID2 = icontentController:addSubComponent(helloComponent2:getIComponent())
-	
-	local internalFacetList = {
-			{id = membershipID1, name = "IHello1"},
-			{id = membershipID2, name = "IHello2"}
-			}
-	icontentController:bindFacet(internalFacetList, utils.replication, "IHelloX")
-		
+  -- Cria o componente composto
+  local componentId = { name = "ComplexHello", major_version = 1, minor_version = 0, patch_version = 0, platform_spec = "" }
+  local component = ComponentContext(orb, componentId)
+  local icontentController = component:getFacetByName(utils.ICONTENTCONTROLLER_NAME).facet_ref
+
+  -- Adiciona os componentes Hello no Componente Composto e cria uma faceta.
+  local membershipID1 = icontentController:addSubComponent(helloComponent1:getIComponent())
+  local membershipID2 = icontentController:addSubComponent(helloComponent2:getIComponent())
+
+  local internalFacetList = {
+      {id = membershipID1, name = "IHello1"},
+      {id = membershipID2, name = "IHello2"}
+      }
+  icontentController:bindFacet(internalFacetList, utils.replication, "IHelloX")
+
   -- publishes the IComponent facet's IOR to a file. We could publish any facet,
-  -- since the _component() exists to obtain the IComponent facet, and any 
+  -- since the _component() exists to obtain the IComponent facet, and any
   -- other facet from it. This step can also be replaced by other methods of
   -- publishing, like a name server.
   oil.writeto("server.ior", tostring(component:getIComponent()))

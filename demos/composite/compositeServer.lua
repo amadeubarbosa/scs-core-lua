98f32b6e68c5d11d2b3e651f2d443b9aa4cc6c20
Mauricio Arieira Rosas <marosas@tecgraf.puc-rio.br>
2013-02-16 18:42:48 +0000

    Foi removido a operacao que cria um connector automaticamente. Com isso foi necessario adaptar o demo para que o proprio servidor crie o conector. -- Nao foi necessario alterar o cliente.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/scs/core/lua/branches/SCS_CORE_LUA_COMPOSITE_2012_10_06@137989 ae0415b3-e90b-0410-900d-d0be9363c56b

--- demos/composite/compositeServer.lua
+++ demos/composite/compositeServer.lua
@@ -8,13 +8,28 @@ utils = utils()
 
 Log:level(5)
 
---implementação da faceta IHello
+-- Implementação da faceta IHello
 local Hello = oo.class{}
 function Hello:__new(name)
   return oo.rawnew(self,{name = name})
 end
 function Hello:sayHello()
   print("Hello " .. self.name .. "!")
+end
+
+-- Conector da faceta Hello
+local HelloConnector = oo.class()
+
+function HelloConnector:__new(helloFacets)
+  return oo.rawnew(self,{facets = helloFacets})
+end
+
+function HelloConnector:sayHello()
+  local orb = self.context._orb
+  for _,facet in ipairs(self.facets) do
+    facet = orb:narrow(facet, helloFacetInterface)
+    facet:sayHello()
+  end
 end
 
 -- OiL configuration
@@ -28,14 +43,22 @@ oil.main(function()
   orb:loadidlfile(idlPath .. "/hello.idl")
   oil.newthread(orb.run, orb)
 
-  -- Cria dois componentes Hello
-  local componentId = { name = "Hello1", major_version = 1, minor_version = 0, patch_version = 0, platform_spec = "" }
+  -- Cria dois componentes Hello e o conector
+  local componentId = { name = "Hello", major_version = 1, minor_version = 0, patch_version = 0, platform_spec = "" }
   local helloComponent1 = ComponentContext(orb, componentId)
   helloComponent1:addFacet("IHello1", helloFacetInterface, Hello("SubComponent One"))
 
-  local componentId = { name = "Hello2", major_version = 1, minor_version = 0, patch_version = 0, platform_spec = "" }
+  componentId = { name = "Hello", major_version = 1, minor_version = 0, patch_version = 0, platform_spec = "" }
   local helloComponent2 = ComponentContext(orb, componentId)
   helloComponent2:addFacet("IHello2", helloFacetInterface, Hello("SubComponent Two"))
+
+  local helloFacets = {}
+  table.insert(helloFacets, helloComponent1:getIComponent():getFacetByName("IHello1"))
+  table.insert(helloFacets, helloComponent2:getIComponent():getFacetByName("IHello2"))
+
+  componentId = { name = "HelloConnector", major_version = 1, minor_version = 0, patch_version = 0, platform_spec = "" }
+  local helloConnector = ComponentContext(orb, componentId)
+  helloConnector:addFacet("IHello", helloFacetInterface, HelloConnector(helloFacets))
 
   -- Cria o componente composto
   local componentId = { name = "ComplexHello", major_version = 1, minor_version = 0, patch_version = 0, platform_spec = "" }
@@ -45,12 +68,9 @@ oil.main(function()
   -- Adiciona os componentes Hello no Componente Composto e cria uma faceta.
   local membershipID1 = icontentController:addSubComponent(helloComponent1:getIComponent())
   local membershipID2 = icontentController:addSubComponent(helloComponent2:getIComponent())
+  local membershipID3 = icontentController:addSubComponent(helloConnector:getIComponent())
 
-  local internalFacetList = {
-      {id = membershipID1, name = "IHello1"},
-      {id = membershipID2, name = "IHello2"}
-      }
-  icontentController:bindFacet(internalFacetList, utils.replication, "IHelloX")
+  icontentController:bindFacet(membershipID3, "IHello", "IHelloX")
 
   -- publishes the IComponent facet's IOR to a file. We could publish any facet,
   -- since the _component() exists to obtain the IComponent facet, and any

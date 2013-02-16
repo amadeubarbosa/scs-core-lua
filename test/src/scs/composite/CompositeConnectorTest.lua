7865dfac7601a145192e4c2995d599ae4f1d68c6
Mauricio Arieira Rosas <marosas@tecgraf.puc-rio.br>
2013-02-16 18:55:39 +0000

    formatacao
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/scs/core/lua/branches/SCS_CORE_LUA_COMPOSITE_2012_10_06@137990 ae0415b3-e90b-0410-900d-d0be9363c56b

--- test/src/scs/composite/CompositeConnectorTest.lua
+++ test/src/scs/composite/CompositeConnectorTest.lua
@@ -14,21 +14,21 @@ local receptacleName = "HelloReceptacle"
 --implementação da faceta IHello
 local Hello = oo.class{}
 function Hello:__new(name)
-	return oo.rawnew(self,{name = name})
+  return oo.rawnew(self,{name = name})
 end
 function Hello:sayHello()
-	print("Hello " .. self.name .. "!")
+  print("Hello " .. self.name .. "!")
 end
 
 --implementação do conector IHello
 local ConnectorHello = oo.class{}
 function ConnectorHello:__new()
-	return oo.rawnew(self,{})
+  return oo.rawnew(self,{})
 end
 function ConnectorHello:sayHello()
   local receptacle =  self.context:getReceptacleByName(receptacleName)
   if receptacle == nil then return end
-  
+
   for _,connection in pairs(receptacle.connections) do
     connection.objref:sayHello()
   end
@@ -40,7 +40,7 @@ local helloFacetInterface = "IDL:scs/demos/helloworld/Hello:1.0"
 
 
 oil.main(function()
-	local idlPath = os.getenv("IDL_PATH")
+  local idlPath = os.getenv("IDL_PATH")
   orb:loadidlfile(idlPath .. "/scs.idl")
   orb:loadidlfile(idlPath .. "/composite.idl")
   orb:loadidlfile(idlPath .. "/hello.idl")
@@ -51,8 +51,8 @@ oil.main(function()
   -- Component3 = Componente que ficará fora do componente composto
   local componentId = { name = "Hello1", major_version = 1, minor_version = 0, patch_version = 0, platform_spec = "" }
   local helloComponent1 = ComponentContext(orb, componentId)
-  helloComponent1:addFacet("IHello1", helloFacetInterface, Hello("SubComponent One"))	
-  
+  helloComponent1:addFacet("IHello1", helloFacetInterface, Hello("SubComponent One"))
+
   local componentId = { name = "Hello2", major_version = 1, minor_version = 0, patch_version = 0, platform_spec = "" }
   local helloComponent2 = ComponentContext(orb, componentId)
   helloComponent2:addFacet("IHello2", helloFacetInterface, Hello("SubComponent Two"))
@@ -67,40 +67,40 @@ oil.main(function()
   helloConnector:addFacet("IHello", helloFacetInterface, ConnectorHello())
   helloConnector:addReceptacle(receptacleName, helloFacetInterface, true)
 
-	-- Cria o componente composto
-	local componentId = { name = "ComplexHello", major_version = 1, minor_version = 0, patch_version = 0, platform_spec = "" }
-	local component = ComponentContext(orb, componentId)
-	local icontentController = component:getFacetByName(utils.ICONTENTCONTROLLER_NAME).facet_ref
+  -- Cria o componente composto
+  local componentId = { name = "ComplexHello", major_version = 1, minor_version = 0, patch_version = 0, platform_spec = "" }
+  local component = ComponentContext(orb, componentId)
+  local icontentController = component:getFacetByName(utils.ICONTENTCONTROLLER_NAME).facet_ref
 
   -- Adiciona o conector
   local membershipIDConnector = icontentController:addSubComponent(helloConnector:getIComponent())
-   
-	-- Adiciona os componentes Hello no Componente Composto e cria uma faceta.
-	local membershipID1 = icontentController:addSubComponent(helloComponent1:getIComponent())
-	local membershipID2 = icontentController:addSubComponent(helloComponent2:getIComponent())
-	
+
+  -- Adiciona os componentes Hello no Componente Composto e cria uma faceta.
+  local membershipID1 = icontentController:addSubComponent(helloComponent1:getIComponent())
+  local membershipID2 = icontentController:addSubComponent(helloComponent2:getIComponent())
+
   -- Conecta os subcomponentes no Conector.
-  local helloReceptacle = helloConnector:getFacetByName(utils.IRECEPTACLES_NAME)  
-  helloReceptacle = orb:narrow(helloReceptacle.facet_ref, utils.IRECEPTACLES_INTERFACE) 
+  local helloReceptacle = helloConnector:getFacetByName(utils.IRECEPTACLES_NAME)
+  helloReceptacle = orb:narrow(helloReceptacle.facet_ref, utils.IRECEPTACLES_INTERFACE)
   helloReceptacle:connect(receptacleName, helloComponent1:getFacetByName("IHello1").facet_ref)
   helloReceptacle:connect(receptacleName, helloComponent2:getFacetByName("IHello2").facet_ref)
-  
+
   -- Tenta conectar o componente que está fora no componente composto no conector
   local ok, errorMsg = pcall(helloReceptacle.connect, helloReceptacle, receptacleName, helloComponent3:getFacetByName("IHello3").facet_ref)
   if ok then
     error("Operacao nao permitida. Excecao deveria ser sido lancada")
   elseif errorMsg._repid ~= idl.throw.InvalidConnection then
     error("Excecao deveria ser ".. idl.throw.InvalidConnection .. " e foi " .. errorMsg._repid)
-  end  
-  
-	-- Verificando que o componentes foram criados
-	local membershipDescription = icontentController:getSubComponents()
-	for _, desc in pairs(membershipDescription) do 
-		local iComponent = orb:narrow(desc.iComponent, utils.ICOMPONENT_INTERFACE)
-		print(string.format("  MembershipID = %s | ComponentID.Name = %s", desc.id, iComponent:getComponentId().name))
-	end
-  
-	local bindingID = icontentController:bindConnectorFacet(membershipIDConnector, "IHello", "IHelloX")
+  end
+
+  -- Verificando que o componentes foram criados
+  local membershipDescription = icontentController:getSubComponents()
+  for _, desc in pairs(membershipDescription) do
+    local iComponent = orb:narrow(desc.iComponent, utils.ICOMPONENT_INTERFACE)
+    print(string.format("  MembershipID = %s | ComponentID.Name = %s", desc.id, iComponent:getComponentId().name))
+  end
+
+  local bindingID = icontentController:bindConnectorFacet(membershipIDConnector, "IHello", "IHelloX")
 
   print "\nImprime os bindings cridos"
   local bindDescList = icontentController:retrieveBindings()
@@ -108,12 +108,12 @@ oil.main(function()
     local entityType = bindDesc.isFacet and "Faceta" or "Receptaculo"
     print(string.format("%s: %s|id = %s",entityType, bindDesc.name, bindDesc.id))
   end
-  
+
   print("\nTesta se o conector está funcionando corretamente.")
   local connectorFacet = component:getFacetByName("IHelloX").facet_ref
   connectorFacet = orb:narrow(connectorFacet, helloFacetInterface)
   connectorFacet:sayHello()
-  
+
   print "\nRemovendo o Conector"
   local facetList = component.IMetaInterface:getFacets()
   local ok = icontentController:unbind(bindingID)
@@ -123,7 +123,7 @@ oil.main(function()
   else
     print("** Conector não removido.")
   end
-  
+
   Log:info("Teste realizado com Sucesso.")
   os.exit(0)
 end)
